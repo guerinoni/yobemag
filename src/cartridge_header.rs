3bfd2b181663a658692ff5a330f32ae7a3df0c7d
Federico Guerinoni <guerinoni.federico@gmail.com>
2021-08-29 16:05:16 +0200

    Implement check on nintendo logo in byte
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/cartridge_header.rs
+++ src/cartridge_header.rs
@@ -2,6 +2,22 @@ pub struct CartridgeHeader {
     title: String,
 }
 
+const NINTENDO_LOGO: [u8; 48] = [
+    0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B, 0x03, 0x73, 0x00, 0x83, 0x00, 0x0C, 0x00, 0x0D,
+    0x00, 0x08, 0x11, 0x1F, 0x88, 0x89, 0x00, 0x0E, 0xDC, 0xCC, 0x6E, 0xE6, 0xDD, 0xDD, 0xD9, 0x99,
+    0xBB, 0xBB, 0x67, 0x63, 0x6E, 0x0E, 0xEC, 0xCC, 0xDD, 0xDC, 0x99, 0x9F, 0xBB, 0xB9, 0x33, 0x3E,
+];
+
+fn check_logo(data: &Vec<u8>) -> Result<(), std::io::Error> {
+    match data[0x104..0x134].iter().cmp(NINTENDO_LOGO.iter()) {
+        std::cmp::Ordering::Equal => Ok(()),
+        std::cmp::Ordering::Less | std::cmp::Ordering::Greater => Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "logo bytes are corrupted.",
+        )),
+    }
+}
+
 impl CartridgeHeader {
     pub fn new(data: &Vec<u8>) -> Result<Self, std::io::Error> {
         let t = data[0x134..0x144]
@@ -10,11 +26,12 @@ impl CartridgeHeader {
             .map(|&c| c)
             .collect::<Vec<_>>();
 
+        check_logo(&data)?;
+
         Ok(CartridgeHeader {
             title: String::from_utf8(t).unwrap(),
         })
     }
-
 }
 
 #[cfg(test)]
@@ -30,5 +47,13 @@ mod tests {
         let header = CartridgeHeader::new(&data.unwrap());
         assert_eq!(header.is_err(), false);
         assert_eq!(header.unwrap().title, "TETRIS");
+    }
+
+    #[test]
+    fn verify_nintendo_logo() {
+        let data = fs::read("./roms/Tetris.gb");
+        assert_eq!(data.is_err(), false);
+        let header = CartridgeHeader::new(&data.unwrap());
+        assert_eq!(header.is_err(), false);
     }
 }
