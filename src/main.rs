5a5ca5ff0db1d4edc41538fbb243bf8b75195da1
Federico Guerinoni <guerinoni.federico@gmail.com>
2021-08-29 15:52:14 +0200

    Return error if no arg passed when start from CLI
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+use std::env;
+
+fn main() -> Result<(), std::io::Error> {
     println!("starting yobemag...");
+
+    let args = env::args().collect::<Vec<_>>();
+    if args.len() < 2 {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "missing rom as first arg.",
+        ));
+    }
+
+    let rom = &args[1];
+    println!("load of {}", &rom);
+
+    Ok(())
 }
