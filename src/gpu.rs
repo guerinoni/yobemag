d65139294e7222101e8902bccac2731e76e5d2ee
Federico Guerinoni <guerinoni.federico@gmail.com>
2021-09-29 22:43:43 +0200

    Add control byte for GPU
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/gpu.rs
+++ src/gpu.rs
@@ -25,6 +25,16 @@ pub struct GPU {
     scroll_y: u8,
     /// scroll X 0xFF43 (read-write)
     scroll_x: u8,
+
+    /// Bit 0 - BG/Window Display/Priority     (0=Off, 1=On)
+    /// Bit 1 - OBJ (Sprite) Display Enable    (0=Off, 1=On)
+    /// Bit 2 - OBJ (Sprite) Size              (0=8x8, 1=8x16)
+    /// Bit 3 - BG Tile Map Display Select     (0=0x9800-0x9BFF, 1=0x9C00-0x9FFF)
+    /// Bit 4 - BG & Window Tile Data Select   (0=0x8800-0x97FF, 1=0x8000-0x8FFF)
+    /// Bit 5 - Window Display Enable          (0=Off, 1=On)
+    /// Bit 6 - Window Tile Map Display Select (0=0x9800-0x9BFF, 1=0x9C00-0x9FFF)
+    /// Bit 7 - LCD Display Enable             (0=Off, 1=On)
+    control: u8,
 }
 
 impl GPU {
@@ -34,6 +44,7 @@ impl GPU {
             status: 0,
             scroll_y: 0,
             scroll_x: 0,
+            control: 0,
         }
     }
 }
@@ -41,6 +52,7 @@ impl GPU {
 impl ReadWrite for GPU {
     fn contains(self: &Self, address: usize) -> bool {
         (0x8000..=0x1FFF).contains(&address)
+            || 0xFF40 == address
             || 0xFF41 == address
             || 0xFF42 == address
             || 0xFF43 == address
@@ -49,6 +61,7 @@ impl ReadWrite for GPU {
     fn read_byte(self: &Self, address: usize) -> Result<u8, std::io::Error> {
         match address {
             0x8000..=0x9FFF => Ok(self.vram[address - 0x8000]),
+            0xFF40 => Ok(self.control),
             0xFF41 => Ok(self.status),
             0xFF42 => Ok(self.scroll_y),
             0xFF43 => Ok(self.scroll_x),
@@ -66,6 +79,7 @@ impl ReadWrite for GPU {
     fn write_byte(self: &mut Self, address: usize, value: u8) -> Result<(), std::io::Error> {
         match address {
             0x8000..=0x9FFF => Ok(self.vram[address - 0x8000] = value),
+            0xFF40 => Ok(self.control = value),
             0xFF41 => Ok(self.status = value),
             0xFF42 => Ok(self.scroll_y = value),
             0xFF43 => Ok(self.scroll_x = value),
