46f03b6e1ebf14a1a423b3cc1bc2d6b8c7588174
Federico Guerinoni <guerinoni.federico@gmail.com>
2021-10-13 22:33:37 +0200

    Implement conversion CpuFlag to u8
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/register.rs
+++ src/register.rs
@@ -1,3 +1,51 @@
+use std::cmp::PartialEq;
+use std::ops::BitXor;
+use std::ops::Shl;
+
+pub struct CpuFlag {
+    pub carry: bool,
+    pub half_carry: bool,
+    pub negative: bool,
+    pub zero: bool,
+}
+
+impl CpuFlag {
+    pub fn to_u8(self: &Self) -> u8 {
+        let bits = [
+            self.zero as u8,
+            self.negative as u8,
+            self.half_carry as u8,
+            self.carry as u8,
+            0,
+            0,
+            0,
+            0,
+        ];
+        convert(&bits).unwrap()
+    }
+}
+
+#[derive(Debug)]
+pub enum ConversionError {
+    Overflow,
+    NonBinaryInput,
+}
+
+pub fn convert<T: PartialEq + From<u8> + BitXor<Output = T> + Shl<Output = T> + Clone>(
+    bits: &[u8],
+) -> Result<T, ConversionError> {
+    if bits.len() > (std::mem::size_of::<T>() * 8) {
+        return Err(ConversionError::Overflow);
+    }
+    if bits.iter().filter(|&&bit| bit != 0 && bit != 1).count() > 0 {
+        return Err(ConversionError::NonBinaryInput);
+    }
+
+    Ok(bits.iter().fold(T::from(0), |result, &bit| {
+        (result << T::from(1)) ^ T::from(bit)
+    }))
+}
+
 pub struct Registers {
     pub a: u8,
     pub flags: CpuFlag,
@@ -9,13 +57,6 @@ pub struct Registers {
     pub l: u8,
     pub program_counter: i32,
     pub stack_pointer: u16,
-}
-
-pub struct CpuFlag {
-    pub carry: bool,
-    pub half_carry: bool,
-    pub negative: bool,
-    pub zero: bool,
 }
 
 impl Registers {
@@ -67,5 +108,77 @@ impl Registers {
     pub fn hl(self: &Self) -> u16 {
         let ret = (self.h as u16) << 8;
         ret | self.l as u16
+    }
+
+    pub fn af(self: &Self) -> u16 {
+        let ret = (self.a as u16) << 8;
+        ret | self.flags.to_u8() as u16
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{CpuFlag, Registers};
+
+    #[test]
+    fn verify_conversion_flag() {
+        let flags = CpuFlag {
+            carry: false,
+            half_carry: false,
+            negative: false,
+            zero: false,
+        };
+
+        assert_eq!(flags.to_u8(), 0);
+
+        let flags = CpuFlag {
+            carry: false,
+            half_carry: false,
+            negative: false,
+            zero: true,
+        };
+
+        assert_eq!(flags.to_u8(), 128);
+
+        let flags = CpuFlag {
+            carry: false,
+            half_carry: false,
+            negative: true,
+            zero: true,
+        };
+
+        assert_eq!(flags.to_u8(), 192);
+
+        let flags = CpuFlag {
+            carry: false,
+            half_carry: true,
+            negative: true,
+            zero: true,
+        };
+
+        assert_eq!(flags.to_u8(), 224);
+
+        let flags = CpuFlag {
+            carry: true,
+            half_carry: true,
+            negative: true,
+            zero: true,
+        };
+
+        assert_eq!(flags.to_u8(), 240);
+    }
+
+    #[test]
+    fn verify_af() {
+        let mut r = Registers::new();
+        r.flags = CpuFlag {
+            carry: true,
+            half_carry: true,
+            negative: true,
+            zero: true,
+        };
+
+        dbg!(r.a, r.flags.to_u8());
+        assert_eq!(r.af(), 496);
     }
 }
